8a3be94fb64c7118996a0a89e0e9e9a9450e0ab7
S25088 <S25088@pjwstk.edu.pl>
2024-05-21 11:00:29 +0200

    Dodanie kolekcji do modelu

--- APBD-EF/Models/Client.cs
+++ APBD-EF/Models/Client.cs
@@ -5,5 +5,6 @@ public class Client
     public int Id { get; set; }
     public string Name { get; set; }
     public string Pesel { get; set; }
+    public ICollection<ClientTrip> ClientTrips { get; set; }
     
 }
