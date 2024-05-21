c158fba9c530e79c3d534ee15c2339462ed2dac1
S25088 <S25088@pjwstk.edu.pl>
2024-05-21 10:57:40 +0200

    Dodanie modelu ClientTrip

---
+++ APBD-EF/Models/ClientTrip.cs
@@ -0,0 +1,11 @@
+﻿namespace APBD_EF.Models;
+
+public class ClientTrip
+{
+    public Trip Trip { get; set; }
+    public Client Client { get; set; }
+    public int TripId { get; set; }
+    public int ClientId { get; set; }
+    public DateTime RegisteredAt { get; set; }
+    public DateTime? PaymentDate { get; set; }
+}
