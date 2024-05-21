2362cd3045c3c7d9e672eb626256b69a284eb59f
S25088 <S25088@pjwstk.edu.pl>
2024-05-21 10:54:59 +0200

    dodanie modelu Trip

---
+++ APBD-EF/Models/Trip.cs
@@ -0,0 +1,9 @@
+﻿namespace APBD_EF.Models;
+
+public class Trip
+{ 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+}
