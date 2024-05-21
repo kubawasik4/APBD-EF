7988b69f032dc8eb8b1f7da9248d09321f61ac39
S25088 <S25088@pjwstk.edu.pl>
2024-05-21 11:14:15 +0200

    Dodanie dto

---
+++ APBD-EF/DTOS/ClientsDTO.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD_EF.DTOS;
+
+public class ClientsDTO
+{
+    public int IdClient { get; set; }
+    public string Name { get; set; }
+    public string Pesel { get; set; }
+    public IEnumerable<TripDTO> Trips { get; set; }
+}
+public class TripDTO
+{
+    public int IdTrip { get; set; }
+    public string Name { get; set; }
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public IEnumerable<ClientsDTO> Clients { get; set; }
+}
+
+public class CreateClientDTO
+{
+    [Required] public string Name { get; set; }
+    [Required] public string Pesel { get; set; }
+}
+
+public class CreateTripDTO
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public DateTime DateFrom { get; set; }
+    [Required]
+    public DateTime DateTo { get; set; }
+}
