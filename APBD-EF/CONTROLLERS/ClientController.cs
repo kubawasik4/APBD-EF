881ceaaf4844a38c03efb62c26b8d2bf6d2e098b
S25088 <S25088@pjwstk.edu.pl>
2024-05-23 11:42:46 +0200

    przypisanie klienta

--- APBD-EF/CONTROLLERS/ClientController.cs
+++ APBD-EF/CONTROLLERS/ClientController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Data.Entity;
 using APBD_EF.CONTEXT;
+using APBD_EF.DTOS;
+using APBD_EF.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APBD_EF.CONTROLLERS;
+
 [Route("api/[controller]")]
 [ApiController]
 public class ClientController : ControllerBase
@@ -34,4 +37,33 @@ public class ClientController : ControllerBase
         return Ok();
 
     }
+
+    [HttpPost("{idTrip}/clients")]
+    public async Task<IActionResult> PrzypiszKlienta(int id, [FromBody] DTOS.CreateClientDTO dto)
+    {
+        var trip = await _context.Trip.FindAsync(id);
+        if (trip == null)
+        {
+            return NotFound();
+        }
+
+        var client = await _context.Client.SingleOrDefaultAsync(e => e.Pesel == dto.Pesel);
+        if (client == null)
+        {
+            _context.Client.Add(new Client { Pesel = dto.Pesel, Name = dto.Name });
+            await _context.SaveChangesAsync();
+        }
+
+        var tripExist = await _context.ClientTrips.AnyAsync(e => e.TripId == id && e.ClientId == client.Id);
+        if (tripExist)
+        {
+            return BadRequest();
+        }
+
+        var clTrip = new ClientTrip { TripId = id, ClientId = client.Id, RegisteredAt = DateTime.Now };
+        _context.ClientTrips.Add(clTrip);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
 }
+
