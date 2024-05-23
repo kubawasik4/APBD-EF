baa7acfef2c319d9d35b95182917662545b157df
S25088 <S25088@pjwstk.edu.pl>
2024-05-23 11:32:07 +0200

    usuniecie klienta

--- APBD-EF/CONTROLLERS/ClientController.cs
+++ APBD-EF/CONTROLLERS/ClientController.cs
@@ -1,4 +1,5 @@
-﻿using APBD_EF.CONTEXT;
+﻿using System.Data.Entity;
+using APBD_EF.CONTEXT;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APBD_EF.CONTROLLERS;
@@ -11,5 +12,26 @@ public class ClientController : ControllerBase
     public ClientController(Context context)
     {
         _context = context;
+    }
+
+    [HttpDelete("{id")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var client = await _context.Client.FindAsync(id);
+        if (client == null)
+        {
+            return NotFound();
+        }
+
+        var trips = await _context.ClientTrips.AnyAsync((e => e.ClientId == id));
+        if (trips)
+        {
+            return BadRequest();
+        }
+
+        _context.Client.Remove(client);
+        await _context.SaveChangesAsync();
+        return Ok();
+
     }
 }
